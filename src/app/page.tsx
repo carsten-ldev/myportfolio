a923ed41d9c072ac85962521a878d5c8a12c8648
Carsten <clu@rts.dk>
2025-02-23 16:21:20 +0100

    fixed dependency link and escaped single quotes on projects page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import { Link } from "next-view-transitions"; 
+import Link from "next/link";
 import { FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 export default function Home() {
