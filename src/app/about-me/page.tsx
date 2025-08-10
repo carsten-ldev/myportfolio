6c10d3dcefacc77dbe815c664e9a8c98e1f98459
clurts <clu@rts.dk>
2025-08-10 11:07:57 +0200

    fixed responsivity and margin

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function ProfilePage() {
 
     return (
-      <main className="flex flex-col gap-4 w-4/5 mx-auto mt-12">
+      <main className="flex flex-col gap-4 w-4/5 mx-auto my-12">
         <h2 className="font-mono font-medium text-4xl md:mb-3"><span className="text-[#6CE0D6] tracking-widest mr-1">#</span>About me</h2>
 
         <p className="font-sans text-xl leading-relaxed md:w-4/5 xl:w-3/5">
@@ -14,11 +14,11 @@ export default function ProfilePage() {
         <p className="font-sans text-xl leading-relaxed md:w-4/5 xl:w-3/5">I enjoy creating visually appealing user interfaces that are performant, accessible, and easy to use. </p>
         <p className="font-sans text-xl leading-relaxed md:w-4/5 xl:w-3/5">I’m particularly curious about how AI is transforming the way we work in frontend development &ndash; shaping not only our workflows and the solutions we build, but also how users will interact with the web in the future.</p>
 
-        <h3 className="font-mono text-2xl font-semibold mt-4 mb-2">Feel free to reach out</h3>
-       <p className="text-lg font-normal tracking-wide flex mb-4">
-        <FaEnvelope size="1.5rem" className="mr-2" /> hello@carsten-l.dev
-       <FaLinkedin size="1.5rem" className="mr-2 ml-8" /><Link href="https://linkedin.com/in/carsten-l" target="blank">linkedin.com/in/carsten-l</Link> 
-       <FaGithub size="1.5rem" className="mr-2 ml-8" /> <Link href="https://github.com/clurts" target="blank">github.com/clurts</Link> 
+        <h3 className="font-mono text-2xl font-semibold mt-4 mb-2 text-balance">Feel free to reach out</h3>
+       <p className="text-lg font-normal tracking-wide flex flex-col lg:flex-row gap-2 lg:gap-8 mb-4">
+        <span className="flex"><FaEnvelope size="1.5em" className="mr-2" /> hello@carsten-l.dev</span>
+       <span className="flex"><FaLinkedin size="1.5em" className="mr-2" /><Link href="https://linkedin.com/in/carsten-l" target="blank">linkedin.com/in/carsten-l</Link></span> 
+       <span className="flex"><FaGithub size="1.5em" className="mr-2" /> <Link href="https://github.com/clurts" target="blank">github.com/clurts</Link> </span>
        </p>
         
        
